975586f24cc6569017cacc4a89d5ff4b099487fc
vitalydevelop <9gr6jjg5d7@privaterelay.appleid.com>
2026-07-15 21:25:58 +0200

    Add files via upload

---
+++ mapy-autocomplet.js
@@ -0,0 +1,6 @@
+const MAPY_API_KEY = "4DYLKZ2XHhnDW_lFW9KUgwa24a80lIMzi6jtd4fMr70";
+
+const placeInput = document.getElementById("placeInput");
+const deliveryInput = document.getElementById("deliveryInput");
+
+console.log("Mapy Autocomplete initialized");
